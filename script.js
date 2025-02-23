3a05c9bbee9cc0b39b18b89901f4a494775748f0
Johann Erazo <89018154+JoeErazo@users.noreply.github.com>
2025-02-24 06:32:34 +0800

    Enable modal open/close toggle with add button

--- script.js
+++ script.js
@@ -23,3 +23,17 @@ addBookToLibrary("To Kill a Mockingbird", "Harper Lee", 336, false);
 addBookToLibrary("The Alchemist", "Paulo Coelho", 208, true);
 addBookToLibrary("The Catcher in the Rye", "J.D. Salinger", 288, false);
 addBookToLibrary("Mrs. Frisby and the Rats of NIMH", "Robert C. O'Brien", 233, true);
+
+const modal = document.querySelector("#modal");
+const addBookButton = document.querySelector(".add-book");
+const formAdd = document.querySelector("#add");
+const formCancel = document.querySelector("#cancel");
+
+addBookButton.addEventListener("click", function() {
+  modal.showModal();
+});
+
+formCancel.addEventListener("click", function(e) {
+  e.preventDefault();
+  modal.close();
+});
