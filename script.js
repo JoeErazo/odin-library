bffc581fdc9dc201ea321c6ed537bf1a126ebcab
Johann Erazo <89018154+JoeErazo@users.noreply.github.com>
2025-02-24 01:49:52 +0800

    Create book array, constructor and add function
    Also created some default library contents

---
+++ script.js
@@ -0,0 +1,25 @@
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.info = function() {
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${
+              this.read ? "already read" : "not yet read"}`;
+  };
+}
+
+function addBookToLibrary(title, author, pages, read) {
+  myLibrary.push(new Book(title, author, pages, read));
+}
+
+function displayBooks() {
+
+}
+
+addBookToLibrary("To Kill a Mockingbird", "Harper Lee", 336, false);
+addBookToLibrary("The Alchemist", "Paulo Coelho", 208, true);
+addBookToLibrary("The Catcher in the Rye", "J.D. Salinger", 288, false);
+addBookToLibrary("Mrs. Frisby and the Rats of NIMH", "Robert C. O'Brien", 233, true);
