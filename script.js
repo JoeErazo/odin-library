68bb96cb9a52e0a3b43cddb80361b6d43710cdbc
Johann Erazo <89018154+JoeErazo@users.noreply.github.com>
2025-02-24 10:36:24 +0800

    Enable "Add Book" functionality

--- script.js
+++ script.js
@@ -24,16 +24,41 @@ addBookToLibrary("The Alchemist", "Paulo Coelho", 208, true);
 addBookToLibrary("The Catcher in the Rye", "J.D. Salinger", 288, false);
 addBookToLibrary("Mrs. Frisby and the Rats of NIMH", "Robert C. O'Brien", 233, true);
 
-const modal = document.querySelector("#modal");
 const addBookButton = document.querySelector(".add-book");
+const modal = document.querySelector("#modal");
+const formTitle = document.querySelector("#title");
+const formAuthor = document.querySelector("#author");
+const formPageCount = document.querySelector("#page-count");
+const formRead = document.querySelector("is-read");
 const formAdd = document.querySelector("#add");
 const formCancel = document.querySelector("#cancel");
+const cardContainer = document.querySelector(".card-container");
 
 addBookButton.addEventListener("click", function() {
   modal.showModal();
+  modal.returnValue = ""; // prevent unexpected addition of card
 });
 
 formCancel.addEventListener("click", function(e) {
   e.preventDefault();
   modal.close();
+});
+
+modal.addEventListener("close", function() {
+  // check validity of form submission before adding card
+  // success: returnValue = "add"; failed: returnValue = ""
+  if(modal.returnValue === "add") {
+    let newCard = document.createElement("div");
+    newCard.classList.add("card");
+    newCard.innerHTML = `
+      <div class="book-cover"></div>
+      <div class="book-details">
+        <h2 class="book-title">${formTitle.value}</h2>
+        <h3 class="author">${formAuthor.value}</h3>
+        <p class="page-count">${formPageCount.value}</p>
+        <p class="read">${formRead ? "Read" : "Not Read"}</p>
+      </div>
+    `;
+    cardContainer.appendChild(newCard);
+  }
 });
