e4de1ddaafd6325c81d612efca83051928bb7893
Johann Erazo <89018154+JoeErazo@users.noreply.github.com>
2025-03-02 01:21:34 +0800

    Privatize variables and functions

--- script.js
+++ script.js
@@ -1,110 +1,111 @@
-const myLibrary = [];
-const addBookButton = document.querySelector(".add-book");
-const modal = document.querySelector("#modal");
-const formTitle = document.querySelector("#title");
-const formAuthor = document.querySelector("#author");
-const formPageCount = document.querySelector("#page-count");
-const formRead = document.querySelector("#is-read");
-const formAdd = document.querySelector("#add");
-const formCancel = document.querySelector("#cancel");
-const cardContainer = document.querySelector(".card-container");
+(function() {
+  const myLibrary = [];
+  const addBookButton = document.querySelector(".add-book");
+  const modal = document.querySelector("#modal");
+  const formTitle = document.querySelector("#title");
+  const formAuthor = document.querySelector("#author");
+  const formPageCount = document.querySelector("#page-count");
+  const formRead = document.querySelector("#is-read");
+  const formAdd = document.querySelector("#add");
+  const formCancel = document.querySelector("#cancel");
+  const cardContainer = document.querySelector(".card-container");
 
-class Book {
-  constructor(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+  class Book {
+    constructor(title, author, pages, read) {
+      this.title = title;
+      this.author = author;
+      this.pages = pages;
+      this.read = read;
+    }
+
+    get info() {
+      return `${this.title} by ${this.author}, ${this.pages} pages, ${
+        this.read ? "already read" : "not yet read"}`;
+    }
   }
 
-  get info() {
-    return `${this.title} by ${this.author}, ${this.pages} pages, ${
-      this.read ? "already read" : "not yet read"}`;
+  function addBookToLibrary(title, author, pages, read) {
+    myLibrary.push(new Book(title, author, pages, read));
   }
-}
 
-function addBookToLibrary(title, author, pages, read) {
-  myLibrary.push(new Book(title, author, pages, read));
-}
-
-function displayBooks() {
-  let cardsHTML = "";
-  myLibrary.forEach((book) => {
-    cardsHTML += `
-      <div class="card">
-        <button class="remove-card">☓</button>
-        <div class="book-cover"></div>
-        <div class="book-details">
-          <h2 class="book-title">${book.title}</h2>
-          <h3 class="author">${book.author}</h3>
-          <p class="page-count">${book.pages}</p>
-          <p class="read">${book.read ? "Read" : "Not Read"}</p>
+  function displayBooks() {
+    let cardsHTML = "";
+    myLibrary.forEach((book) => {
+      cardsHTML += `
+        <div class="card">
+          <button class="remove-card">☓</button>
+          <div class="book-cover"></div>
+          <div class="book-details">
+            <h2 class="book-title">${book.title}</h2>
+            <h3 class="author">${book.author}</h3>
+            <p class="page-count">${book.pages}</p>
+            <p class="read">${book.read ? "Read" : "Not Read"}</p>
+          </div>
+          <button class="toggle-read">${book.read ? "🗸" : "᠆"}</button>
         </div>
-        <button class="toggle-read">${book.read ? "🗸" : "᠆"}</button>
-      </div>
-    `;
-  });
-  cardContainer.innerHTML = cardsHTML;
+      `;
+    });
+    cardContainer.innerHTML = cardsHTML;
 
-  // enable remove button functionality on existing cards
-  document.querySelectorAll(".remove-card").forEach((button) => enableRemoveButton(button));
-  document.querySelectorAll(".toggle-read").forEach((button) => enableToggleRead(button));
-}
-
-function enableRemoveButton(button) {
-  button.addEventListener("click", function() {
-    button.closest("div.card").remove();
-  });
-}
-
-function enableToggleRead(button) {
-  button.addEventListener("click", function() {
-    const readContainer = button
-                            .closest("div.card")
-                            .querySelector("p.read");
-    readContainer.textContent =
-      readContainer.textContent === "Read" ? "Not Read" : "Read";
-    button.textContent = button.textContent === "🗸" ? "᠆" : "🗸";
-  });
-}
-
-addBookButton.addEventListener("click", function() {
-  modal.showModal();
-  modal.returnValue = ""; // prevent unexpected addition of card
-  // reset form input values
-  formTitle.value = null;
-  formAuthor.value = null;
-  formPageCount.value = "";
-  formRead.checked = false;
-});
-
-formCancel.addEventListener("click", function(e) {
-  e.preventDefault();
-  modal.close();
-});
-
-modal.addEventListener("close", function() {
-  // check validity of form submission before adding card
-  // success: returnValue = "add"; failed: returnValue = ""
-  if(modal.returnValue === "add") {
-    addBookToLibrary(formTitle.value, formAuthor.value, formPageCount.value, formRead.checked);
-    displayBooks();
+    // enable remove button functionality on existing cards
+    document.querySelectorAll(".remove-card").forEach((button) => enableRemoveButton(button));
+    document.querySelectorAll(".toggle-read").forEach((button) => enableToggleRead(button));
   }
-});
 
-modal.addEventListener("keydown", function(e) {
-  if(e.key==="Enter"){
+  function enableRemoveButton(button) {
+    button.addEventListener("click", function() {
+      button.closest("div.card").remove();
+    });
+  }
+
+  function enableToggleRead(button) {
+    button.addEventListener("click", function() {
+      const readContainer = button
+                              .closest("div.card")
+                              .querySelector("p.read");
+      readContainer.textContent =
+        readContainer.textContent === "Read" ? "Not Read" : "Read";
+      button.textContent = button.textContent === "🗸" ? "᠆" : "🗸";
+    });
+  }
+
+  addBookButton.addEventListener("click", function() {
+    modal.showModal();
+    modal.returnValue = ""; // prevent unexpected addition of card
+    // reset form input values
+    formTitle.value = null;
+    formAuthor.value = null;
+    formPageCount.value = "";
+    formRead.checked = false;
+  });
+
+  formCancel.addEventListener("click", function(e) {
     e.preventDefault();
-    modal.returnValue = "add";
     modal.close();
-  }
-});
+  });
 
+  modal.addEventListener("close", function() {
+    // check validity of form submission before adding card
+    // success: returnValue = "add"; failed: returnValue = ""
+    if(modal.returnValue === "add") {
+      addBookToLibrary(formTitle.value, formAuthor.value, formPageCount.value, formRead.checked);
+      displayBooks();
+    }
+  });
 
-// initialize myLibrary with default content
-addBookToLibrary("To Kill a Mockingbird", "Harper Lee", 336, false);
-addBookToLibrary("The Alchemist", "Paulo Coelho", 208, true);
-addBookToLibrary("The Catcher in the Rye", "J.D. Salinger", 288, false);
-addBookToLibrary("Mrs. Frisby and the Rats of NIMH", "Robert C. O'Brien", 233, true);
+  modal.addEventListener("keydown", function(e) {
+    if(e.key==="Enter"){
+      e.preventDefault();
+      modal.returnValue = "add";
+      modal.close();
+    }
+  });
 
-displayBooks();
+  // initialize myLibrary with default content
+  addBookToLibrary("To Kill a Mockingbird", "Harper Lee", 336, false);
+  addBookToLibrary("The Alchemist", "Paulo Coelho", 208, true);
+  addBookToLibrary("The Catcher in the Rye", "J.D. Salinger", 288, false);
+  addBookToLibrary("Mrs. Frisby and the Rats of NIMH", "Robert C. O'Brien", 233, true);
+
+  displayBooks();
+})();
